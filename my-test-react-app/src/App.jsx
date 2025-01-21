9587324b1b0d1cc481a575ec64b98897610c5bf1
Avinash <avinashrana55@yahoo.com>
2025-01-21 17:57:57 +0530

    Updated App component to handle renderising of list
    Used terneray as well as && operator for rendering of list
    with the use of chaining

--- my-test-react-app/src/App.jsx
+++ my-test-react-app/src/App.jsx
@@ -1,31 +1,44 @@
-function List(props){
-  if(!props.animals){
-    return <div>Loading...</div>;
-  }
+// function List(props){
+  
+//   return(
+//   <>
+//     {!props.animals?
+//       (<div>Loading...</div>):
+//       props.animals.length > 0 ?
+//       <ul>
+//         {props.animals.map(animal => <li key={animal}>{animal}</li>)}
+//       </ul>:
+//       (<div>There is no animals in list</div>)}
+//   </>
+//   );
+// }
 
-  if(props.animals.length === 0){
-    return <div>Thers is no animals in list</div>
-  }
+//or
+function List(props){
 
   return(
-  <ul>
-    {props.animals.map((animal) => {
-      return <li key={animal}>{animal}</li>;
-    })}
-  </ul>
-  );
+    <>
+      {!props.animals && <div>Loading...</div>}
+      {props.animals && props.animals.length > 0 && (
+        <ul>
+          {props.animals.map(animal => <li key={animal}>{animal}</li>)}
+        </ul>
+      )}
+      {props.animals && props.animals.length === 0 && <div>There is no animals in list</div>}
+    </>
+    );
 }
-
 
 function App() {
 
-  //const animals = ["Lion", "tiger", "monkey", "cheetah", "giraffe"];
-  const animals = [];
+  
+  const animals = ["Lion", "tiger", "monkey", "cheetah", "giraffe"];
+  //const animals = [];
 
   return (
     <div>
     <h1>Animal</h1>
-      <List animals={animals} />
+      <List  animals={animals}/>
     </div>
   )
 }
